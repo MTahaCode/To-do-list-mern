2ec1b80441639e182a73b28df19fa7e16048ed68
MTahaCode <taha.makhdoom10@gmail.com>
2025-04-30 21:04:14 +0500

    seperated pod and deployemetn yaml files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   build: {
     outDir: "dist",
-  }
+  },
+  server: {
+    proxy: {
+      '/todos': {
+        target: 'http://backend-service:5000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
